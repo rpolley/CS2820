7f82dc7fe8117bcac3211da125196457d838e91a
Anani Noupougbe <Anani Noupougbe>
2016-12-05 00:04:01 -0600

    add some codes

--- oo-warehouse/production/production/MoveBelt.java
+++ oo-warehouse/production/production/MoveBelt.java
@@ -1,14 +1,13 @@
 package production;
 
-import java.awt.List;
 import java.awt.Point;
-import java.util.*;
+import java.util.ArrayList;
 	/**
 	 * 
 	 * @author Anani
 	 *
 	 */
-public class MockBelt implements Belt{		
+public class MoveBelt implements Belt{		
 		Floor n;
 		Packer pa;
 		Picker pi;
@@ -19,13 +18,13 @@ public class MockBelt implements Belt{
 		//Call Floor object
 		//to locate the belt area (place),cells and other
 		//Initialisation Bin to null
-	public MockBelt(Floor n,String[] beltarea, Packer pa, Picker pi){
+	public MoveBelt(Floor n,ArrayList<Point> beltarea, Packer pa, Picker pi){
 		this.n = n;
 		this.pa = pa;
 		this.pi = pi;
 		beltarea = n.getBeltLocs();
 		binMade = null;
-		L=beltarea.length;  		
+		L=beltarea.size();  		
  		//this.nC=nC
  		this.beltarea=new String[L]; 
 		System.arraycopy(beltarea, 0, this.beltarea, 0, L); 		
@@ -42,54 +41,59 @@ public class MockBelt implements Belt{
 	public void tick(int count){
 		
 		if (binMade!= null){
-			if (!binMade.isFinished())return;
-			int m = pa.getPackerp();
-			if (m!=1)return; //Packer present
-			
-			Cell c = n.getCell(pa.getPacker());
+			if (!binMade.isFinished())return;			
+			pi.Picker[L][0]="B";
+			/*Cell c = n.getCell(pa.getPacker());
 			if (c.getContents()!=null)return;
-			c.setContents(binMade);
+			c.setContents(binMade);*/
 			binMade = null;		
 		}
 		if (!isMovable())return;
+		beltarea[L-140]="B";
+		pi.Picker[L][0]=null;
+		doPacker();
 		
 	}
 	private boolean isMovable(){
 		
-		if (binMade != null)return false;	
-		for (String p: beltarea){
-			Cell c= n.getCell(p);
+		if (binMade != null)return false;
+		int m = pa.getPackerp();//check if
+		if (m!=1)return false; //Packer is present		
+		for (int i=0; i<L; i++){
+			if (beltarea[i]!= null)return false;			
+		}
+		int r = pa.getStatePacker();
+		if (r!=1)return false;
+		int u = pi.getStatePicker();
+		if (u!=1)return false;		
+		return true;
+
+		/*for (String p: beltarea){
+			//Cell c= n.getCell(p);
+			String[] c=  beltarea;
 			Object o  = c.getContents();
 			if (o==null);
 			if ((o instanceof Bin) && !((Bin)o).isFinished())return false;
 			if ((o instanceof Parcel) && !((Parcel)o).isFinished())return false;
-		}
-		return true;
+		}*/
 	}
 
 	private void doPacker(){
 		
 		String[][] W= new String[L][0];
-		int[] action=new int[]{1,2,3};
- 		int k;
+		int[] action=new int[]{1,2,3,4}; 		
  		
  		for(int i=0; i<L; i++){ 
  				System.arraycopy(pa.Packer[i], 0, W[i], 0,L); 
  			}
  		for (int i=0; i<action.length; i++){
  		// Take Bin from the belt
-			if (action[i]==1){ 
- 				k=0; 
- 				while(k>=0){ 
-					if(W[L-140][k]==null){ 
- 						W[L-140][k]=beltarea[L-140]; 
- 						k=-100;						 
-					} 
-				k+=1; 
- 				} 
-				beltarea[L-140]=null; 
- 			}
-			
+			if (action[i]==1){
+				
+					if(W[L-140][0]!=null)return; 
+ 						W[L-140][0]=beltarea[L-140];
+ 						beltarea[L-140]=null;
+					}			
 			// Make the Parcel
  			if(action[i]==3){ 		 				 
  					W[L-140][0]=null; 
@@ -98,26 +102,24 @@ public class MockBelt implements Belt{
  			//Put the parcel on the belt
  			if (action[i]==4){ 
 			beltarea[L-140]="P"; 
-			W[L-140][0]=null; 
- 				 
+			W[L-140][0]=null; 				 
  			}
  		}
-		Cell c = n.getCell();
+		/*Cell c = n.getCell();
 		Object o = c.getContents();
 		assert o instanceof Bin;
 		Bin b = (Bin)o;
 		order v = b.getOrder();
 		Parcel m = new Parcel(v.getAddress(), v.getOrderItems());
-		c.setContents(m);
+		c.setContents(m);*/
 	}
 
 	public boolean binAvailable() {
 		if(binMade != null){
 		return false;
 		}
-	Cell c = n.getCell((pi.Picker));
-	if (c.getCell()!=null)	return false;
-	return true;
+		if(pi.Picker!=null)return false;	 
+		return true;
 	}
 
 	public Bin getBin(){
