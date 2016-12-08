f67bf9b98a4c255aa66e965bd15422e183d2873f
ryan <ryan-polley@uiowa.edu>
2016-12-07 18:42:05 -0600

    picker now responds to robot arrival

--- oo-warehouse/production/production/Picker.java
+++ oo-warehouse/production/production/Picker.java
@@ -7,15 +7,19 @@ import java.util.ArrayList;
  * @author Anani
  *
  */
-public class Picker extends PickerSpace{
+public class Picker extends PickerSpace implements FrameListener{
 	private ArrayList<Point> beltarea;
 	public Picker(ArrayList<Point> beltarea, int row, int col){
 		super(row,col);
 		this.beltarea = beltarea; 		
 	}
 	
-
-
+	public void onFrame(){
+		if(Master.master.getRobotScheduler().closestRobot(row, col).arrivedatDestination){
+			processItems();
+		}
+	}
+	
 	public void processItems() {
 		// TODO Auto-generated method stub
 		
