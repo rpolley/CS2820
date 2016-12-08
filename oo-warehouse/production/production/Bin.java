4215d57f64c36435bdb8bdb6a1b3f525f39789f7
ryan <ryan-polley@uiowa.edu>
2016-12-08 16:05:46 -0600

    Revert "Modification on Bin"
    This reverts commit f1ae9f030e6f8d67af6394641520d1194f56f542.

--- oo-warehouse/production/production/Bin.java
+++ oo-warehouse/production/production/Bin.java
@@ -1,26 +1,38 @@
 package production;
 /**
  * 
- * @author Anani
+ * @author rpolley
  * Bin full with Items Ordered
  * and available to push on the belt
  */
 
-public class Bin {	
-	order Order;
-	boolean finished;		
-	public Bin(){	
-		Order = null; finished = false;}
-	public boolean isFinished(){
-		return finished;}
-	public void setFinished(){
-		finished = true;}
-	public order getOrder(){
-		return Order;}
-	public void setOrder(order o){
-		Order = o;}	
-	public String toString(){
-		return "Bin";
+public class Bin {
+	int pos;
+	MockBelt on;//the belt this bin is on
+	//todo: way to hold items
+	public Bin(MockBelt b){
+		on = b;
+		pos = on.beltarea.length-1;
 	}
-
+	public boolean atStart(){
+		return pos==on.beltarea.length-1;
+	}
+	public boolean atEnd(){
+		return  pos==0;
+	}
+	public BeltSpace getPosition(){
+		return on.beltarea[pos];
+	}
+	/*
+	 * @author rpolley
+	 * move the bin down the belt
+	 */
+	public void move(){
+		if(pos > 0){
+			pos--;
+		}else{
+			on.getBin();
+			System.out.println("Got Bin!");
+		}
+	}
 }
