3f63ac1c0e9a8df04d541666f47a5ac2c4c735dd
Anani Noupougbe <Anani Noupougbe>
2016-12-08 13:30:16 -0600

    some code added

--- oo-warehouse/production/production/Packer.java
+++ oo-warehouse/production/production/Packer.java
@@ -1,22 +1,30 @@
 package production;
 
+import java.awt.Point;
 import java.util.ArrayList;
 
 /**
  * 
  * @author Anani
- * bad code removed by rpolley
+ *
  */
-public class Packer extends PackerSpace{
- 	public  ArrayList<Point> beltarea;
- 	
- 	public Packer(ArrayList<Point> beltarea, int row, int col){
- 		super(row, col);
-		this.beltarea = beltarea;
- 	}
-	public void processItems() {
-		// TODO Auto-generated method stub
-		
-	}
+public class Packer extends PackerSpace{ 
+  	public  ArrayList<Point> beltarea; 
+ 	Floor F; 
+ 	public Packer(ArrayList<Point> beltarea, int row, int col){ 
+  		super(row, col); 
+ 		this.beltarea = beltarea; 
+ 	} 
+ 	public void processItems() { 
+ 		Cell c = Cell(F.getPackLcs()); 
+ 		Object o = c.getContents();  // get what the Cell has in it 
+ 	 	assert o instanceof Bin;     // it had better be a Bin 
+ 	 	Bin b = (Bin)o;              // use the Bin to 
+ 	 	order v = b.getOrder();      // get the finished Order 
+ 	 	Parcel n = new Parcel(v.getAddress(),v.getOrderItems()); 
+ 	 	c.setContents(n);  // replace Bin with Parcel on the belt 
 
-}
+ 	} 
+ 
+ 
+ } 
