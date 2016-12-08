acc1d82ddf769208a68b3ace363aaab4d8707a99
ryan <ryan-polley@uiowa.edu>
2016-12-08 16:08:30 -0600

    fixing some errors

--- oo-warehouse/production/production/Packer.java
+++ oo-warehouse/production/production/Packer.java
@@ -2,16 +2,21 @@ package production;
 
 import java.util.ArrayList;
 
-public class Packer extends PackerSpace {
-	public ArrayList<Point> beltarea;
-
-	public Packer(ArrayList<Point> beltarea, int row, int col) {
-		super(row, col);
+/**
+ * 
+ * @author Anani
+ * bad code removed by rpolley
+ */
+public class Packer extends PackerSpace{
+ 	public  ArrayList<Point> beltarea;
+ 	
+ 	public Packer(ArrayList<Point> beltarea, int row, int col){
+ 		super(row, col);
 		this.beltarea = beltarea;
-	}
-
+ 	}
 	public void processItems() {
 		// TODO Auto-generated method stub
+		
 	}
 
 }
